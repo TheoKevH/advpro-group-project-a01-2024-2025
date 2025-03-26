ac376c04b5e21edb5cc866cd222b6ae7ebeea302
TheoKevH <theodorekevinh@gmail.com>
2025-03-26 15:30:16 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/BuildingstoreApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.buildingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingstoreApplication.class, args);
+    }
+
+}
