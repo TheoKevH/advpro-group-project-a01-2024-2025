788730aa75d35a008dcc2601e2923c3cab72c605
dikbayy <dicky.bayusadewo@gmail.com>
2025-04-11 20:42:28 +0700

    [FIX][TEST] Update SupplierService test using ArgumentCaptor for field comparison

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
@@ -2,17 +2,17 @@ package id.ac.ui.cs.advprog.buildingstore.supplier_management.service;
 
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.dto.SupplierDTO;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
-import id.ac.ui.cs.advprog.buildingstore.supplier_management.factory.SupplierFactory;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.repository.SupplierRepository;
-
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.SupplierService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class SupplierServiceTest {
@@ -25,11 +25,21 @@ class SupplierServiceTest {
 
     @Test
     void addSupplier_shouldSaveSupplierToRepository() {
-        SupplierDTO dto = new SupplierDTO("PT Sumber Jaya", "Jakarta", "0812345678", "Elektronik");
-        Supplier expectedSupplier = SupplierFactory.fromDTO(dto);
+        // Arrange
+        SupplierDTO dto = new SupplierDTO("PT Maju", "Bandung", "08123456789", "Elektronik");
 
+        // Act
         supplierService.addSupplier(dto);
 
-        verify(supplierRepository, times(1)).save(expectedSupplier);
+        // Assert
+        ArgumentCaptor<Supplier> captor = ArgumentCaptor.forClass(Supplier.class);
+        verify(supplierRepository, times(1)).save(captor.capture());
+
+        Supplier savedSupplier = captor.getValue();
+
+        assertEquals("PT Maju", savedSupplier.getName());
+        assertEquals("Bandung", savedSupplier.getAddress());
+        assertEquals("08123456789", savedSupplier.getContact());
+        assertEquals("Elektronik", savedSupplier.getCategory());
     }
 }
