a756d70dc5469d63d2226ed30124fe3a2654d0c6
TheoKevH <theodorekevinh@gmail.com>
2025-04-11 21:18:58 +0700

    test: for deployment

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
@@ -14,5 +14,34 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class SupplierServiceTest {
+
+    /*@Mock
+    private SupplierRepository supplierRepository;
+
+    @InjectMocks
+    private SupplierService supplierService;
+
+    @Test
+    void addSupplier_shouldSaveSupplierToRepository() {
+        // Arrange
+        SupplierDTO dto = new SupplierDTO("PT Maju", "Bandung", "08123456789", "Elektronik");
+
+        // Act
+        supplierService.addSupplier(dto);
+
+        // Assert
+        ArgumentCaptor<Supplier> captor = ArgumentCaptor.forClass(Supplier.class);
+        verify(supplierRepository, times(1)).save(captor.capture());
+
+        Supplier savedSupplier = captor.getValue();
+
+        assertEquals("PT Maju", savedSupplier.getName());
+        assertEquals("Bandung", savedSupplier.getAddress());
+        assertEquals("08123456789", savedSupplier.getContact());
+        assertEquals("Elektronik", savedSupplier.getCategory());
+    }
+
+     */
 }
