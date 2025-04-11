63bb8cd2ea099acc41d666f269f8911725bef4f8
dikbayy <dicky.bayusadewo@gmail.com>
2025-04-11 20:24:52 +0700

    [RED] Add test for SupplierService addSupplier feature

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/service/SupplierServiceTest.java
@@ -1,5 +1,35 @@
 package id.ac.ui.cs.advprog.buildingstore.supplier_management.service;
 
-public class SupplierServiceTest {
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.dto.SupplierDTO;
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.factory.SupplierFactory;
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.repository.SupplierRepository;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SupplierServiceTest {
+
+    @Mock
+    private SupplierRepository supplierRepository;
+
+    @InjectMocks
+    private SupplierService supplierService;
+
+    @Test
+    void addSupplier_shouldSaveSupplierToRepository() {
+        SupplierDTO dto = new SupplierDTO("PT Sumber Jaya", "Jakarta", "0812345678", "Elektronik");
+        Supplier expectedSupplier = SupplierFactory.fromDTO(dto);
+
+        supplierService.addSupplier(dto);
+
+        verify(supplierRepository, times(1)).save(expectedSupplier);
+    }
 }
