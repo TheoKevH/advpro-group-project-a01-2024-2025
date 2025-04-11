5e788fc70873fc36940e9a01cf645a82f9ea5e39
Adam Ghaviyasha <adamghaviyasha@gmail.com>
2025-04-11 16:13:15 +0700

    fix: autowiring issues

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/controller/PaymentController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/controller/PaymentController.java
@@ -1,10 +1,5 @@
 package id.ac.ui.cs.advprog.buildingstore.controller;
 
-import id.ac.ui.cs.advprog.buildingstore.service.PaymentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import id.ac.ui.cs.advprog.buildingstore.model.Payment;
 import id.ac.ui.cs.advprog.buildingstore.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
