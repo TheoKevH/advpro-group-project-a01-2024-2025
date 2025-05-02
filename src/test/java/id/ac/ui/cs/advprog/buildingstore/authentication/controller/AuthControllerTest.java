7f820c9f85b8e092ac0909297abc30d3086372f7
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-02 18:49:36 +0700

    [RED] Add test for POST /register to save user and redirect to login

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthControllerTest.java
@@ -1,19 +1,27 @@
 package id.ac.ui.cs.advprog.buildingstore.authentication.controller;
 
+import id.ac.ui.cs.advprog.buildingstore.authentication.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean private UserRepository userRepository;
+    @MockBean private PasswordEncoder passwordEncoder;
 
     @Test
     public void loginPage_ShouldReturnOkAndContainLoginText() throws Exception {
@@ -28,6 +36,18 @@ public class AuthControllerTest {
         mockMvc.perform(get("/register"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Register")));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void registerUser_ShouldSaveUserAndRedirectToLogin() throws Exception {
+        mockMvc.perform(post("/register")
+                        .param("username", "kasir1")
+                        .param("password", "kasirpass")
+                        .param("role", "CASHIER")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
     }
 
 }
