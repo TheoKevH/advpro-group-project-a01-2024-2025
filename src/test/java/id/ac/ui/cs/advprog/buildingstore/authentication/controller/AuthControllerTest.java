768638456652a3f09d5c99285bf9cb60cd512752
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-02 18:34:36 +0700

    [RED] Add test for rendering register page

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.security.test.context.support.WithMockUser;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -21,4 +21,13 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("username")));
     }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void registerPage_ShouldReturnOkAndContainRegisterForm() throws Exception {
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Register")));
+    }
+
 }
