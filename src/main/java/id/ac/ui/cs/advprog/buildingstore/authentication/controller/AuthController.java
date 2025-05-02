7b3f2070e930273f54190e2dd01242d920fd84ed
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-02 18:38:25 +0700

    [GREEN] Implement controller and basic template for register page

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/AuthController.java
@@ -1,7 +1,8 @@
 package id.ac.ui.cs.advprog.buildingstore.authentication.controller;
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import id.ac.ui.cs.advprog.buildingstore.authentication.dto.RegisterRequest;
 
 @Controller
 public class AuthController {
@@ -10,4 +11,11 @@ public class AuthController {
     public String loginPage() {
         return "login";
     }
+
+    @GetMapping("/register")
+    public String registerPage(Model model) {
+        model.addAttribute("registerRequest", new RegisterRequest());
+        return "register";
+    }
+
 }
---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/authentication/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.buildingstore.authentication.dto;
+import id.ac.ui.cs.advprog.buildingstore.authentication.model.Role;
+
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private Role role;
+
+    // Getters & Setters
+}
