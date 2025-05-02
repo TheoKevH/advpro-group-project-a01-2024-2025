68e64dd693a7114b7d972f00169273da130d2dd7
FrankyRay <frankyraymarcellsinaga@gmail.com>
2025-05-02 16:59:21 +0700

    add: customer

---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/customer/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package id.ac.ui.cs.advprog.buildingstore.customer.controller;
+
+import id.ac.ui.cs.advprog.buildingstore.customer.model.Customer;
+import id.ac.ui.cs.advprog.buildingstore.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/register")
+    public String registerCustomerPage(Model model) {
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "registerCustomer";
+    }
+
+    @PostMapping("/register")
+    public String registerCustomerPost(@ModelAttribute Customer customer, Model model) {
+        customerService.addCustomer(customer);
+        return "redirect:list";
+    }
+
+    @GetMapping("/list")
+    public String listCustomers(Model model) {
+        List<Customer> customers = customerService.getAllCustomers();
+        model.addAttribute("customers", customers);
+        return "listCustomers";
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/customer/model/Customer.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.buildingstore.customer.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Customer {
+    private String customerId;
+    private String customerName;
+    private String customerEmail;
+}
