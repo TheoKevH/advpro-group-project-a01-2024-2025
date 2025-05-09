cb66f7a20f9576d7ef2b83ecf3715e718b056c90
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-09 17:57:08 +0700

    [GREEN] Implement SupplierController with GET /list for JSON response and POST /add for supplier creation and redirect

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/SupplierController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/SupplierController.java
@@ -1,15 +1,15 @@
 package id.ac.ui.cs.advprog.buildingstore.supplier_management.controller;
 
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.dto.SupplierDTO;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.SupplierService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/supplier")
 @RequiredArgsConstructor
 public class SupplierController {
@@ -17,7 +17,14 @@ public class SupplierController {
     private final SupplierService supplierService;
 
     @GetMapping("/list")
+    @ResponseBody
     public List<Supplier> getAllSuppliers() {
         return supplierService.getAllSuppliers();
+    }
+
+    @PostMapping("/add")
+    public String addSupplier(@ModelAttribute SupplierDTO dto) {
+        supplierService.addSupplier(dto);
+        return "redirect:/supplier/list";
     }
 }
