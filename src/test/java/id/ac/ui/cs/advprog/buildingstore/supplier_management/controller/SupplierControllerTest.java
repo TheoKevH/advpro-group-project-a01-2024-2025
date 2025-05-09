26ecaf2b296645bfee27ce5b8ae9d643ecbd35d9
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-09 13:21:28 +0700

    [RED] Add test for SupplierController to return all suppliers as JSON

---
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/SupplierControllerTest.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.buildingstore.supplier_management.controller;
+
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.SupplierService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.*;
+
+@WebMvcTest(SupplierController.class)
+class SupplierControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SupplierService supplierService;
+
+    @Test
+    void getAllSuppliers_shouldReturnListAsJson() throws Exception {
+        List<Supplier> mockSuppliers = List.of(
+                Supplier.builder().id(1L).name("Supplier A").build(),
+                Supplier.builder().id(2L).name("Supplier B").build()
+        );
+
+        Mockito.when(supplierService.getAllSuppliers()).thenReturn(mockSuppliers);
+
+        mockMvc.perform(get("/supplier/list"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", is(2)))
+                .andExpect(jsonPath("$[0].name", is("Supplier A")))
+                .andExpect(jsonPath("$[1].name", is("Supplier B")));
+    }
+}
