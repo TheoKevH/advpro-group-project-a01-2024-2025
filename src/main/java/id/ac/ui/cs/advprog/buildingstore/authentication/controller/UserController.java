5bd7b1e20154f396027e74d4b14ab9179069246d
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-23 17:16:47 +0700

    [FEAT] Add delete user endpoint in UserController

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/UserController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/authentication/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -37,8 +38,20 @@ public class UserController {
             model.addAttribute("registerRequest", request);
             return "admin/register_user";
         }
-        authService.register(request);
+        try {
+            authService.register(request);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("registerRequest", request);
+            model.addAttribute("errorMessage", e.getMessage());
+            return "admin/register_user";
+        }
+
         return "redirect:/admin/users";
     }
 
+    @GetMapping("/admin/users/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        authService.deleteUser(id);
+        return "redirect:/admin/users";
+    }
 }
