4f667fa521d588b6e9b2fea6d9631e87ea23b11f
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-23 18:36:22 +0700

    [REFACTOR] Improve readability of showSupplierTransactions controller method

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/TransactionController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/TransactionController.java
@@ -2,12 +2,15 @@ package id.ac.ui.cs.advprog.buildingstore.supplier_management.controller;
 
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.dto.PurchaseTransactionDTO;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
+import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.PurchaseTransaction;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.PurchaseTransactionService;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.SupplierService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/supplier")
@@ -18,11 +21,12 @@ public class TransactionController {
     private final SupplierService supplierService;
 
     @GetMapping("/{id}/transactions")
-    public String showSupplierTransactions(@PathVariable Long id, Model model) {
-        Supplier supplier = supplierService.findById(id);
-        var transactions = transactionService.getTransactionsBySupplier(supplier);
+    public String showSupplierTransactions(@PathVariable("id") Long supplierId, Model model) {
+        Supplier supplier = supplierService.findById(supplierId);
+        List<PurchaseTransaction> transactions = transactionService.getTransactionsBySupplier(supplier);
         model.addAttribute("supplier", supplier);
         model.addAttribute("transactions", transactions);
         return "admin/supplier_transactions";
     }
+
 }
