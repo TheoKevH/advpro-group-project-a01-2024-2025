f32bf8c7439c816d5637ca7fb370b9875f2282ce
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-23 14:00:30 +0700

    [REFACTOR] Refactor SupplierController to use SupplierDTO for both add and edit supplier

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/SupplierController.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/controller/SupplierController.java
@@ -5,9 +5,8 @@ import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.service.SupplierService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/supplier")
@@ -16,27 +15,42 @@ public class SupplierController {
 
     private final SupplierService supplierService;
 
-    @GetMapping("/list")
-    @ResponseBody
-    public List<Supplier> getAllSuppliers() {
-        return supplierService.getAllSuppliers();
+    @GetMapping
+    public String showSupplierList(Model model) {
+        model.addAttribute("suppliers", supplierService.getAllSuppliers());
+        return "admin/supplier_list";
+    }
+
+    @GetMapping("/add")
+    public String showAddSupplierForm(Model model) {
+        model.addAttribute("supplierDTO", new SupplierDTO());
+        return "admin/add_supplier";
     }
 
     @PostMapping("/add")
     public String addSupplier(@ModelAttribute SupplierDTO dto) {
         supplierService.addSupplier(dto);
-        return "redirect:/supplier/list";
+        return "redirect:/supplier";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditSupplierForm(@PathVariable Long id, Model model) {
+        Supplier supplier = supplierService.findById(id);
+        SupplierDTO dto = SupplierDTO.fromEntity(supplier);
+        model.addAttribute("supplierDTO", dto);
+        model.addAttribute("id", id);
+        return "admin/edit_supplier";
     }
 
     @PostMapping("/edit/{id}")
     public String editSupplier(@PathVariable("id") Long id, @ModelAttribute SupplierDTO dto) {
         supplierService.editSupplier(id, dto);
-        return "redirect:/supplier/list";
+        return "redirect:/supplier";
     }
 
     @PostMapping("/delete/{id}")
     public String deleteSupplier(@PathVariable Long id) {
         supplierService.deleteSupplier(id);
-        return "redirect:/supplier/list";
+        return "redirect:/supplier";
     }
 }
