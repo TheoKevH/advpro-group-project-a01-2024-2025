3de6a5f880c5ebb94ed0a9209e0167dbc0a8b8c3
TheoKevH <theodorekevinh@gmail.com>
2025-05-24 20:31:57 +0700

    [RED] create test for new transactionItem

--- src/test/java/id/ac/ui/cs/advprog/buildingstore/transaksi/service/TransactionServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/buildingstore/transaksi/service/TransactionServiceTest.java
@@ -1,11 +1,15 @@
 package id.ac.ui.cs.advprog.buildingstore.transaksi.service;
 
+import id.ac.ui.cs.advprog.buildingstore.transaksi.enums.TransactionStatus;
 import id.ac.ui.cs.advprog.buildingstore.transaksi.model.Transaction;
+import id.ac.ui.cs.advprog.buildingstore.transaksi.repository.InMemoryTransactionRepository;
 import id.ac.ui.cs.advprog.buildingstore.transaksi.repository.TransactionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -41,4 +45,25 @@ class TransactionServiceTest {
 
         assertEquals("AWAITING_PAYMENT", trx.getStatus().name());
     }
+
+    @Test
+    void testCreateTransaction_withItems_shouldStoreCorrectly() {
+        TransactionService service = new TransactionServiceImpl(new InMemoryTransactionRepository());
+
+        List<TransactionItem> items = List.of(
+                new TransactionItem("prod-12", 2),
+                new TransactionItem("prod-34", 1)
+        );
+
+        String customerId = "cust-789";
+        Transaction trx = service.createTransaction(customerId, items);
+
+        assertNotNull(trx.getId());
+        assertEquals(customerId, trx.getCustomerId());
+        assertEquals(TransactionStatus.IN_PROGRESS, trx.getStatus());
+        assertEquals(2, trx.getItems().size());
+        assertEquals("prod-12", trx.getItems().get(0).getProductId());
+        assertEquals(2, trx.getItems().get(0).getQuantity());
+    }
+
 }
