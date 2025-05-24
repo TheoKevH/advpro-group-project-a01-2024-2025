636489d9a20f4b47e005e97838b9bb828ac39233
dikbayy <dicky.bayusadewo@gmail.com>
2025-05-24 15:04:14 +0700

    [FEAT] update SupplierDTO and PurchaseTransactionDTO structure

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/dto/PurchaseTransactionDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/supplier_management/dto/PurchaseTransactionDTO.java
@@ -1,8 +1,9 @@
 package id.ac.ui.cs.advprog.buildingstore.supplier_management.dto;
 
 import id.ac.ui.cs.advprog.buildingstore.supplier_management.model.Supplier;
-import lombok.*;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -10,9 +11,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PurchaseTransactionDTO {
-    private Supplier supplier;
+    @NotBlank(message = "Product name is required")
+    @Size(max = 100)
     private String productName;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private int quantity;
+
+    @NotNull(message = "Total price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Total price must be greater than 0")
     private BigDecimal totalPrice;
+
+    @NotNull(message = "Date is required")
     private LocalDateTime date;
+
+    @NotNull
+    private Supplier supplier;
 }
