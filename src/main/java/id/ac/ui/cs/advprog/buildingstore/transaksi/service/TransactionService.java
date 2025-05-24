06f1f6a5110f251a7aceeb8b4df041e28b02a89f
TheoKevH <theodorekevinh@gmail.com>
2025-05-24 20:35:03 +0700

    [GREEN] create new method in transactionService

--- src/main/java/id/ac/ui/cs/advprog/buildingstore/transaksi/service/TransactionService.java
+++ src/main/java/id/ac/ui/cs/advprog/buildingstore/transaksi/service/TransactionService.java
@@ -5,10 +5,10 @@ import id.ac.ui.cs.advprog.buildingstore.transaksi.model.Transaction;
 import java.util.List;
 
 public interface TransactionService {
-    Transaction createTransaction();
     Transaction getTransaction(String id);
     List<Transaction> getAllTransactions();
     Transaction moveToPayment(String id);
     Transaction markAsPaid(String id);
     void cancelTransaction(String id);
+    Transaction createTransaction(String customerId, List<TransactionItem> items);
 }
